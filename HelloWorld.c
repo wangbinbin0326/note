6bdec6e65806a03cad02619142f972a6e3a15f94
wangbinbin0326 <wangbinbin0326@gmail.com>
2022-11-27 09:15:48 +0800

    v1

---
+++ HelloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+//分段函数求值
+int main(void)
+{
+    char c;
+    c = '7';
+    printf("%d", isalpha(c));
+    //printf("\nResult when uppercase alphabet is passed: %d", isalpha(c));
+    return 0;
+}
